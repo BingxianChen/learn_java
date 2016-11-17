00a648a056be801c46ade7a9a50242991a0a8f3b
Uzumaki-C <15951858526@163.com>
2016-11-17 17:11:37 +0800

    HashMap

---
+++ Array,List/Stories from Templates Module/Using and Improving GladLibs/solve/solve/CodonCount.java
@@ -0,0 +1,91 @@
+
+/**
+ * Write a description of CodonCount here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+public class CodonCount {
+    private HashMap<String,Integer> codons;
+    public CodonCount(){
+        codons = new HashMap<String,Integer>();
+    }
+    
+    public void buildCodonMap(int start, String dna){
+        codons.clear();
+        String threeCodon;
+        for (int i = start; i < dna.length() - 2; i += 3){
+            threeCodon = dna.substring(i,i+3);
+            if (codons.containsKey(threeCodon)){
+                codons.put(threeCodon,codons.get(threeCodon) + 1);
+            }else{
+                codons.put(threeCodon,1);
+            }
+        }  
+    }
+    
+    public String getMostCommonCodon(){
+        StringBuffer commonCodon = new StringBuffer();
+        int maxCount = 0;
+        for(Integer v : codons.values()){
+            if (maxCount < v){
+                maxCount = v;
+            }
+        
+        }        
+        for(String s : codons.keySet()){
+            if (codons.get(s) == maxCount){
+                commonCodon.append(s + " ");
+            }
+        }    
+        return commonCodon.toString();
+    }
+    
+    public void printCodonCounts(int start, int end){
+        for (String s : codons.keySet()){
+            if ((codons.get(s) >= start) && (codons.get(s) <= end)){
+                System.out.println(s + "  " + codons.get(s));           
+            }          
+        }    
+    }
+    
+    public void tester(){
+        FileResource file = new FileResource();
+        String dna = file.asString().toUpperCase().trim();
+        for (int i = 0 ; i <= 2; i++){
+            buildCodonMap(i,dna);
+            int uniqueCodons = codons.size();
+            String commonCodons = getMostCommonCodon();
+            System.out.println("Reading frame starting with " + i + " results in " + uniqueCodons + " unique codons");
+            System.out.println("    and most common codon is " + commonCodons + "with count " + codons.get(commonCodons.substring(0,3)));
+            printCodonCounts(1,5);
+            System.out.println();
+        }
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Array,List/Stories from Templates Module/Using and Improving GladLibs/solve/solve/WordsInFiles.java
@@ -0,0 +1,137 @@
+
+/**
+ * Write a description of WordsInFiles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.util.*;
+import java.io.*;
+public class WordsInFiles {
+    private HashMap<String,ArrayList<String>> maps;
+    public WordsInFiles(){
+        maps = new HashMap<String,ArrayList<String>>();
+    }
+    
+    private void addWordsFromFile(File f){
+        FileResource fr = new FileResource(f);
+        for (String s : fr.words()){
+            if (maps.containsKey(s)){
+                if (maps.get(s).indexOf(f.getName()) == -1){
+                    ArrayList<String> fileList = maps.get(s);
+                    fileList.add(f.getName());
+                    maps.put(s,fileList);
+                }
+            }else{
+                ArrayList<String> fileList = new ArrayList<String>();
+                fileList.add(f.getName());
+                maps.put(s,fileList);
+            }
+        }
+    }
+    
+    public void buildWordFileMap(){
+        maps.clear();
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()){
+            addWordsFromFile(f);
+        }
+    }
+    
+    public int maxNumber(){
+        int max = 0;
+        for(String s : maps.keySet()){
+            if (max < maps.get(s).size()){
+                max = maps.get(s).size();
+            }
+        }
+        return max;
+    }
+    
+    public ArrayList<String> wordsInNumFiles(int number){
+        ArrayList<String> wordList = new ArrayList<String>();
+        for (String s : maps.keySet()){
+            if (number == maps.get(s).size()){
+                wordList.add(s);
+            }
+        }
+        return wordList;
+    }
+    
+    public void printFilesIn(String word){
+        ArrayList<String> fileList = maps.get(word);
+        for (String s : fileList){
+            System.out.println(s);
+        }
+    }
+    
+    public void tester(){
+        buildWordFileMap();
+        int maxNum = maxNumber();
+        System.out.println("maxNum is: " + maxNum);
+        System.out.println("max words is");
+        ArrayList<String> files = wordsInNumFiles(maxNum);
+        for (String s : files){
+            System.out.println(s);
+            printFilesIn(s);
+        }
+        
+        
+        
+        
+        System.out.println("**************************************");
+        StringBuffer sb = new StringBuffer();
+        for (String s : maps.keySet()){
+            sb.delete(0,sb.length());
+            ArrayList<String> fileList = maps.get(s);
+            sb.append(s+":  ");
+            for(String f : fileList){
+                sb.append(f + " ");
+            }
+            System.out.println(sb.toString());
+        }
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
